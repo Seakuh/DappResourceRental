805c54cdcc53e1028e87f11224a2b52798bb788e
DizzleReply <D.grimm@reply.de>
2022-06-08 15:41:07 +0200

    add ResourceAdvert

--- contracts/ResourceRental.sol
+++ contracts/ResourceRental.sol
@@ -18,14 +18,33 @@ contract ResourceRental {
         uint256 rentCount;
     }
 
-    // Model a Renter
+    // Model a Resource
+    struct Resource {
+        uint256 resourceId;
+        string name;
+        string picture;
+        string location;
+    }
 
-    // store renters
+    // Model a Resource Advert
+    struct ResourceAdvert {
+        uint256 advertId;
+        uint256 resourceId;
+        string fromTimeStamp;
+        string toTimeStamp;
+    }
+
+    // Store renters
     // solidity generiert getter
     mapping(uint256 => Renter) public renters;
+    mapping(uint256 => Resource) public resources;
+    mapping(uint256 => ResourceAdvert) public resourcesAdverts;
 
     // Store renters count
     uint256 public rentersCount;
+    uint256 public resourcesCount;
+    uint256 public resourcesAdvertCount;
+
     // Store Rental
 
     // Read Rental
@@ -35,6 +54,36 @@ contract ResourceRental {
         // represent Id of the renter
         rentersCount++;
         renters[rentersCount] = Renter(rentersCount, _name, 0);
+    }
+
+    function addResource(
+        string memory _name,
+        string memory _picture,
+        string memory _location
+    ) private {
+        // represent Id of the renter
+        resourcesCount++;
+        resources[rentersCount] = Resource(
+            resourcesCount,
+            _name,
+            _picture,
+            _location
+        );
+    }
+
+    function addResourceAdvert(
+        uint256 _resourceId,
+        string memory _fromTimeStamp,
+        string memory _toTimeStamp
+    ) private {
+        // represent Id of the renter
+        resourcesAdvertCount++;
+        resourcesAdverts[resourcesAdvertCount] = ResourceAdvert(
+            resourcesCount,
+            _resourceId,
+            _fromTimeStamp,
+            _toTimeStamp
+        );
     }
 
     // Store Rental -> from to free
@@ -47,5 +96,10 @@ contract ResourceRental {
     constructor() public {
         addRenter("Student 1");
         addRenter("Professor 1");
+        addResource("Normal Room", "images/room1.jpeg", "Furtwangen, Germany");
+        addResource("Concert Hall", "images/room1.jpeg", "Mannheim, Germani");
+        addResource("Theater", "images/room1.jpeg", "Rom, Italy");
+        addResource("Server Room", "images/room1.jpeg", "Paris, France");
+        addResource("Test Room", "images/room1.jpeg", "Barcelona, Spain");
     }
 }
