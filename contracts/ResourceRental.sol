c6e9b7f3d6ac109bad34125cc11e95ab646b155e
DizzleReply <D.grimm@reply.de>
2022-08-17 15:49:15 +0200

    refactoring add permission handling to contract add permission dropdown to formular

--- contracts/ResourceRental.sol
+++ contracts/ResourceRental.sol
@@ -3,67 +3,15 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract ResourceRental {
     //----------------------------------------------------------
-    // Structs
-    //----------------------------------------------------------
-
-    // Model a Rental
-    struct Rental {
-        address renterAddress;
-        string resourceName;
-        uint256 bookingId;
-        uint256 resourceId;
-        string startTimestamp;
-        string endTimestamp;
-    }
-
-    // Model a Renter
-    struct Renter {
-        uint256 renterId;
-        string renterName;
-        uint256 rentCount;
-    }
-
-    // Model a Resource
-    struct Resource {
-        uint256 resourceId;
-        string name;
-        string picture;
-        string location;
-        string fromTimeStamp;
-        string toTimeStamp;
-        // uint8[3] requiredTraining;
-    }
-
-    // Model a University
-    struct University {
-        uint256 universityId;
-        string name;
-        address universityAddress;
-    }
-
-    //----------------------------------------------------------
-    // Events
-    //----------------------------------------------------------
-
-    event RenterAdded(string _renterName, address _senderAddress);
-    event ResourceCreated(uint256 _id, address _creatorAddress);
-    event ResourceRented(
-        address _renter,
-        uint256 _resourceId,
-        string _fromTimeStamp,
-        string _toTimestamp
-    );
-    event UniversityAdded();
-
-    //----------------------------------------------------------
     // Storage
     //----------------------------------------------------------
 
     // Store renters
     // solidity generiert getter
-    mapping(uint256 => Renter) public renters;
     mapping(uint256 => Resource) public resources;
-    mapping(uint256 => University) public universities;
+    mapping(address => Permission) public permissionedRenters;
+
+    mapping(address => University) public universities;
     mapping(uint256 => Rental) public rentals;
 
     // Store index
@@ -79,6 +27,74 @@ contract ResourceRental {
     string public candidate;
 
     //----------------------------------------------------------
+    // Structs
+    //----------------------------------------------------------
+
+    // Model a Rental
+    struct Rental {
+        address renterAddress;
+        string resourceName;
+        uint256 bookingId;
+        uint256 resourceId;
+        string startTimestamp;
+        string endTimestamp;
+    }
+
+    // Model a Resource
+    struct Resource {
+        uint256 resourceId;
+        string name;
+        string picture;
+        string location;
+        string fromTimeStamp;
+        string toTimeStamp;
+        Permission permission;
+        // uint8[3] requiredTraining;
+    }
+
+    // Model a University
+    struct University {
+        uint256 universityId;
+        string name;
+        address universityAddress;
+    }
+
+    //----------------------------------------------------------
+    // ENUM
+    //----------------------------------------------------------
+
+    // Permission for Users
+    // For some Resources, the user need to have permissions
+    // these permissions are given by universities
+    enum Permission {
+        ALL,
+        STUDENT,
+        SCIENTIFIC_ASSISTANT,
+        PROFESSOR,
+        ADMIN
+    }
+
+    //----------------------------------------------------------
+    // Events
+    //----------------------------------------------------------
+
+    event RenterAdded(string _renterName, address _senderAddress);
+    event ResourceCreated(uint256 _id, address _creatorAddress);
+    event ResourceRented(
+        address _renter,
+        uint256 _resourceId,
+        string _fromTimeStamp,
+        string _toTimestamp,
+        uint256 _rentalTimeStamp
+    );
+    event UniversityAdded();
+    event PermissionToRenterAdded(
+        uint256 permission,
+        address useraddress,
+        address authority
+    );
+
+    //----------------------------------------------------------
     // private Functions
     //----------------------------------------------------------
 
@@ -87,7 +103,7 @@ contract ResourceRental {
     {
         // represent Id of the University
         universitiesCount++;
-        universities[universitiesCount] = University(
+        universities[msg.sender] = University(
             universitiesCount,
             _name,
             _address
@@ -103,31 +119,32 @@ contract ResourceRental {
         string memory _name,
         address _newUniversityAddress
     ) private {
-        require(msg.sender == universities[_universityId].universityAddress);
+        require(msg.sender == universities[msg.sender].universityAddress);
 
         // represent Id of the University
         universitiesCount++;
-        universities[universitiesCount] = University(
+        universities[msg.sender] = University(
             universitiesCount,
             _name,
             _newUniversityAddress
         );
     }
 
-    function addRenter(string memory _name) public {
-        // represent Id of the renter
-        rentersCount++;
-        renters[rentersCount] = Renter(rentersCount, _name, 0);
-
-        emit RenterAdded(_name, msg.sender);
+    function addPermissionToRenter(address renterAddress, uint256 permission)
+        public
+        _isUniversity
+    {
+        permissionedRenters[renterAddress] = Permission(permission);
+        emit PermissionToRenterAdded(permission, renterAddress, msg.sender);
     }
 
     function addResource(
         string memory _name,
-        string memory _picture,
+        string memory _metaDataLink,
         string memory _location,
         string memory _fromTimeStamp,
-        string memory _toTimeStamp // uint8[] memory _requiredTraining
+        string memory _toTimeStamp,
+        Permission _permission // uint256 _universityId // uint8[] memory _requiredTraining
     ) public {
         // require();
 
@@ -136,10 +153,11 @@ contract ResourceRental {
         resources[resourcesCount] = Resource(
             resourcesCount,
             _name,
-            _picture,
+            _metaDataLink,
             _location,
             _fromTimeStamp,
-            _toTimeStamp
+            _toTimeStamp,
+            _permission
             // _requiredTraining
         );
         emit ResourceCreated(resourcesCount, msg.sender);
@@ -148,8 +166,8 @@ contract ResourceRental {
     // making the function pure or view
     // to call it through web3.js without needing
     // a trancaction -> no gas cost, no confirmation delay
-    function isSenderUniversity(uint256 _id) public view returns (bool) {
-        return msg.sender == universities[_id].universityAddress;
+    function isSenderUniversity() public view returns (bool) {
+        return msg.sender == universities[msg.sender].universityAddress;
     }
 
     function rentResource(uint256 _id) public {
@@ -171,7 +189,8 @@ contract ResourceRental {
             msg.sender,
             _id,
             resource.fromTimeStamp,
-            resource.toTimeStamp
+            resource.toTimeStamp,
+            block.timestamp
         );
     }
 
@@ -180,10 +199,18 @@ contract ResourceRental {
     //----------------------------------------------------------
 
     // isUniversity
-    modifier isUniversity(uint256 _id) {
+    modifier _isUniversity() {
         require(
-            msg.sender == universities[_id].universityAddress,
+            msg.sender == universities[msg.sender].universityAddress,
             "Caller is no university"
+        );
+        _;
+    }
+
+    modifier _hasPermission(Permission permission) {
+        require(
+            permissionedRenters[msg.sender] >= permission,
+            "Caller is no eligible"
         );
         _;
     }
@@ -195,8 +222,8 @@ contract ResourceRental {
     // Constructor
     constructor() public {
         // add example renters
-        addRenter("Student 1");
-        addRenter("Professor 1");
+        // addRenter("Student 1");
+        // addRenter("Professor 1");
 
         // add example resources
         addResource(
@@ -204,7 +231,8 @@ contract ResourceRental {
             "images/room1.jpeg",
             "Furtwangen, Germany",
             "1654782938174",
-            "1655992478797"
+            "1655992478797",
+            Permission.ALL
         );
 
         addResource(
@@ -212,35 +240,40 @@ contract ResourceRental {
             "images/room1.jpeg",
             "Mannheim, Germani",
             "1654782938174",
-            "1655992478797"
+            "1655992478797",
+            Permission.ALL
         );
         addResource(
             "Theater",
             "images/room1.jpeg",
             "Rom, Italy",
             "1654782938174",
-            "1655992478797"
+            "1655992478797",
+            Permission.ALL
         );
         addResource(
             "Server Room",
             "images/room1.jpeg",
             "Paris, France",
             "1654782938174",
-            "1655992478797"
+            "1655992478797",
+            Permission.ALL
         );
         addResource(
             "Test Room",
             "images/room1.jpeg",
             "Barcelona, Spain",
             "1654782938174",
-            "1655992478797"
+            "1655992478797",
+            Permission.ALL
         );
         addResource(
             "Schulungszentrum Darmstadt",
             "https://www.schulungszentrum-darmstadt.de/wp-content/uploads/2019/09/DSC1868.jpg",
             "Darmstadt, Germany",
             "1654782938174",
-            "1655992478797"
+            "1655992478797",
+            Permission.ALL
         );
 
         // add universities who are allowed to create resrouces
